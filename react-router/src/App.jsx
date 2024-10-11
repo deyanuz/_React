0707866aec2504927476ffc0453920eeff9400cf
deyanuz <zunayedofficial@gmail.com>
2024-10-11 13:06:15 +0600

    createBrowserRouter function

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,37 +1,26 @@
 import {
   createBrowserRouter,
-  Routes,
   Route,
-  Link,
-  NavLink,
   createRoutesFromElements,
   RouterProvider,
 } from "react-router-dom";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
+import RootLayout from "./Layouts/RootLayout";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Routes>
+    <Route path="/" element={<RootLayout />}>
       {/* <Route path="/" element={<Home></Home>} /> */}
       {/* above line is equivalent to the line below */}
-      <Route index element={<Home></Home>} />
-      <Route path="about" element={<About></About>} />
-    </Routes>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Route>
   )
 );
 
 function App() {
-  return (
-    // <header>
-    //   <nav>
-    //     <h1>Job Router</h1>
-    //     <NavLink to="/">Home</NavLink>
-    //     <NavLink to="about">About</NavLink>
-    //   </nav>
-    // </header>
-    <RouterProvider></RouterProvider>
-  );
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
